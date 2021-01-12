e06c6dda55175bf4de36712d538cffb296d7e808
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-01-12 17:51:31 -0500

    Manejando enlaces y configuraciones

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import '../assets/styles/components/Header.scss';
+import { Link } from 'react-router-dom';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
+import '../assets/styles/components/Header.scss';
 
 const Header = () => (
   <header className='header'>
-    <img className='header__img' src={logo} alt='Platzi Video' />
+    <Link to='/'>
+      <img className='header__img' src={logo} alt='Platzi Video' />
+    </Link>
     <div className='header__menu'>
       <div className='header__menu--profile'>
         <img src={userIcon} alt='' />
@@ -13,10 +16,10 @@ const Header = () => (
       </div>
       <ul>
         <li>
-          <a href='/'>Cuenta</a>
+          <Link to='/'>Cuenta</Link>
         </li>
         <li>
-          <a href='/'>Cerrar Sesión</a>
+          <Link to='/login'>Cerrar Sesión</Link>
         </li>
       </ul>
     </div>
