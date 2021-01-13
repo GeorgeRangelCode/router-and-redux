ee1b6b58b16ebfb296bcd01a7e67d06387924ea1
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-01-13 11:58:59 -0500

    Crear Formulario de Login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 import '../assets/styles/components/Login.scss';
 
 const Login = () => {
+  const [form, setValues] = useState({
+    email: '',
+  });
+
+  const handleInput = event => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(form);
+  };
+
   return (
     <section className='login'>
       <section className='login__container'>
         <h2>Inicia sesión</h2>
-        <form className='login__container--form'>
-          <input className='input' type='text' placeholder='Correo' />
-          <input className='input' type='password' placeholder='Contraseña' />
-          <button type='button' className='button'>
+        <form className='login__container--form' onSubmit={handleSubmit}>
+          <input
+            name='email'
+            className='input'
+            type='text'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            className='input'
+            type='password'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button type='submit' className='button'>
             Iniciar sesión
           </button>
           <div className='login__container--remember-me'>
