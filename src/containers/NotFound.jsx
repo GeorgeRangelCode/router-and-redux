9e3c65d4da037061dff6197d876c734516dc134c
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-01-14 15:04:38 -0500

    Validaciones

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const NotFound = () => (
   <>
+    <Header />
     <h1>No Encontrado</h1>
     <h2>Regresa al Home</h2>
   </>
