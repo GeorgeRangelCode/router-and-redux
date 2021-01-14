be2909aef6c906d51a909e31d2c6f55f95b54801
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-01-14 08:00:19 -0500

    Uso de gravatar en nuestro proyecto

--- src/utils/gravatar.js
+++ src/utils/gravatar.js
@@ -2,8 +2,8 @@ import md5 from 'md5';
 
 export const gravatar = email => {
   const base = `https://gravatar.com/avatar/`;
-  const formattEmail = email.trim().toLowerCase();
-  const hash = md5(formattEmail, { encoding: 'binary' });
+  const formattedEmail = email.trim().toLowerCase();
+  const hash = md5(formattedEmail, { encoding: 'binary' });
 
   return `${base}${hash}`;
 };
