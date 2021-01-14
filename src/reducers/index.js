5a31b13936dab7b73e7cbb33c48ca001c818270e
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2021-01-14 08:31:07 -0500

    Register con Redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -27,6 +27,12 @@ const reducer = (state, action) => {
         user: action.payload,
       };
 
+    case 'REGISTER_REQUEST':
+      return {
+        ...state,
+        user: action.payload,
+      };
+
     default:
       return state;
   }
